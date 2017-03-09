1cb4625939f1c9ca3291e9386baf0b727279a9f9
Wenxiang Wu <wuwenxiang.sh@gmail.com>
2017-03-09 10:04:18 +0800

    add scripts/TTT-ie-get-http-request.py

---
+++ scripts/TTT-ie-get-http-request.py
@@ -0,0 +1,32 @@
+import pykd
+import re
+
+LOG_FILE = open(r'C:\local\tmp\ttt-ie-http-request.txt', 'w')
+
+def pykdLog2File(logObj, fileObj):
+    for line in str(logObj).split('\n'):
+        fileObj.write(line+'\n')
+
+def pykdLog(log):
+    print(log)
+    pykdLog2File(log, LOG_FILE)
+
+pykd.dbgCommand(r'bc *;g-;bp ieframe!CDownloadWindowItem::_SetState;bp ieframe!CNotificationBar2::SetFormattedText')
+ret = pykd.dbgCommand(r'bl')
+pykdLog(ret)
+
+while True:
+    ret = pykd.dbgCommand(r'g')
+    if 'TTT Replay: End of trace reached.' in ret:
+        pykdLog(ret)
+        break
+    ret = pykd.dbgCommand(r'kP')
+    pykdLog2File(ret)
+    for line in ret.split('\n'):
+        if 'eState = DLState' in line \
+            or 'wchar_t * pwzOriginDownloadUrl = ' in line \
+            or 'wchar_t * pwzDestinationFilePath = ' in line:
+            print(line)
+    LOG_FILE.flush()
+
+LOG_FILE.close()
