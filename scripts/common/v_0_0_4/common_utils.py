dedd1e0fe231123f1300d7e82dffd6c14804ef62
Wu Wenxiang <wuwenxiang.sh@gmail.com>
2017-05-03 13:51:38 +0800

    add common dir

---
+++ scripts/common/v_0_0_4/common_utils.py
@@ -0,0 +1,52 @@
+'''
+Version: 0.0.4
+'''
+
+import pykd
+import re
+
+LOG_FILE = open(r'C:\local\tmp\debugLog.txt', 'w')
+
+def initLog(filePath):
+    global LOG_FILE
+    LOG_FILE = open(filePath, 'w')
+
+def pyLog2File(logObj, fileObj=LOG_FILE):
+    for line in str(logObj).split('\n'):
+        fileObj.write(line+'\n')
+
+def pyLog(log):
+    print(log)
+    pyLog2File(log, LOG_FILE)
+
+def runCmd(cmd):
+    return runCmdLog(cmd, False, False)
+
+def runCmdLog(cmd, cmdVerbose=True, retVerbose=True):
+    cmdLog = pyLog if cmdVerbose else pyLog2File
+    retLog = pyLog if retVerbose else pyLog2File
+    cmdLog('\n> %s\n%s' % (cmd, '-'*20))
+    ret = pykd.dbgCommand(cmd)
+    retLog(ret)
+    return ret
+
+def runCmdGetArgs(cmd, reObj):
+    ret = runCmd(cmd)
+    return [reObj.search(line)for line in ret.split('\n') if reObj.search(line)]
+
+def ttt_test2end(content):
+    if 'TTT Replay: End of trace reached.' in content \
+       or 'TTT Replay: Start of trace reached.' in content:
+        return True
+    return False
+
+def ttt_test2Time(targetTime):
+    ret = pykd.dbgCommand(r'.time')
+    for line in ret.split('\n'):
+        if 'Time Travel Position:' in line:
+            currentTime = line.split()[-1].strip('.')
+            break
+    if int(targetTime, 16) < int(currentTime, 16):
+        return True
+    return False
+
---
+++ scripts/common/v_0_1_0/common_utils.py
@@ -0,0 +1,83 @@
+'''
+Version: 0.1.0
+'''
+
+import pykd
+import re
+
+LOG_FILE_PATH = r'C:\local\tmp\debugLog.txt'
+
+class PyLog:
+    def __init__(self, logFilePath=LOG_FILE_PATH):
+        self.logFilePath = logFilePath
+        self._logFile = open(logFilePath, 'w')
+        
+    def log2File(self, logObj):
+        for line in str(logObj).split('\n'):
+            self._logFile.write(line+'\n')
+
+    def log2Scr(self, logObj):
+        print(logObj)
+
+    def log(self, logObj):
+        self.log2Scr(logObj)
+        self.log2File(logObj)
+
+    def close(self):
+        self._logFile.close()
+
+class Util:
+    def __init__(self, pyLog):
+        self.pyLog = pyLog
+
+    def runCmd(self, cmd):
+        return self.runCmdLog(cmd, False, False)
+
+    def runCmdLog(self, cmd, cmdVerbose=True, retVerbose=True):
+        cmdStr = '\n> %s\n%s' % (cmd, '-'*20)
+        if cmdVerbose:
+            self.pyLog.log2Scr(cmdStr)
+        self.pyLog.log2File(cmdStr)
+        
+        ret = pykd.dbgCommand(cmd)
+        if retVerbose:
+            self.pyLog.log2Scr(ret)
+        self.pyLog.log2File(ret)
+        
+        return ret
+
+    def runCmdGetArgs(self, cmd, reObj):
+        ret = self.runCmd(cmd)
+        return [reObj.search(line)for line in ret.split('\n') if reObj.search(line)]
+
+    @staticmethod
+    def getDumpName():
+        ret = pykd.dbgCommand(r'||')
+        return ret.split('\\')[-1].strip()
+
+    @staticmethod
+    def ttt_test2end(content):
+        if 'TTT Replay: End of trace reached.' in content \
+           or 'TTT Replay: Start of trace reached.' in content:
+            return True
+        return False
+
+    @staticmethod
+    def ttt_test2Time(targetTime):
+        ret = pykd.dbgCommand(r'.time')
+        for line in ret.split('\n'):
+            if 'Time Travel Position:' in line:
+                currentTime = line.split()[-1].strip('.')
+                break
+        if int(targetTime, 16) < int(currentTime, 16):
+            return True
+        return False
+
+if __name__ == '__main__':
+    del LOG_FILE_PATH
+    ret = Util.getDumpName()
+    logPath = r'C:\local\tmp\debugLog-%s.txt' % ret
+    pyLog = PyLog(logPath)
+    util = Util(pyLog)
+    ret = util.runCmd(r'||')
+    pyLog.log(ret)
